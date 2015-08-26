8144033255c376e21eec71e0f84636853d2f071e
Ijem <ij.ofili@gmail.com>
2015-08-26 22:44:39 +0100

    modified files

--- bloc.js
+++ bloc.js
@@ -1,9 +1,11 @@
 function rotdiv(){
 var aArray = [2, 3, 5, 7, 9];
-//var dataStore = {};
 
-var rnd1 = Math.floor(Math.random()*7);
+var rnd1 = aArray[Math.floor(Math.random()*7)];
 var rnd2 = Math.floor(Math.random()*3);
+    if((rnd1 === undefined) || (rnd1 === rnd2)){
+        rnd1 = 3;
+    }
 
 document.getElementById('tleft').innerHTML = rnd1;
 document.getElementById('dleft').innerHTML = rnd2;
