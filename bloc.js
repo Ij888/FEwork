f36029692ab4ffb5145627f136c38cd99836b04b
Ijem <ij.ofili@gmail.com>
2015-08-26 23:07:02 +0100

    modded files

--- bloc.js
+++ bloc.js
@@ -1,10 +1,11 @@
 function rotdiv(){
-var aArray = [2, 3, 5, 7, 9];
+var aArray = ["love", "joy", "peace", "kindness", "self-control"];
 
-var rnd1 = aArray[Math.floor(Math.random()*7)];
-var rnd2 = Math.floor(Math.random()*3);
+var rnd1 = aArray[Math.floor(Math.random()*2)];
+var rnd2 = aArray[Math.floor(Math.random()*2)];
     if((rnd1 === undefined) || (rnd1 === rnd2)){
-        rnd1 = 3;
+        rnd1 = aArray[Math.ceil(Math.random()*3)];
+        
     }
 
 document.getElementById('tleft').innerHTML = rnd1;
